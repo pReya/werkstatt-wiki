d66d74b6594e4704d429f3862e18cc9a816532cd
Moritz Stückler <moritz@bitbetter.de>
2024-02-15 14:41:54 +0100

    fix: updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'makers.hamburg',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
